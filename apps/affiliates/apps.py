5a440875fa04f6e1cf1afa93f37c379c92264191
arunlc <arun.k@spinircle.com>
2025-06-11 12:37:41 +0530

    Create apps.py

---
+++ apps/affiliates/apps.py
@@ -0,0 +1,6 @@
+# apps/affiliates/apps.py
+from django.apps import AppConfig
+
+class AffiliatesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.affiliates'
