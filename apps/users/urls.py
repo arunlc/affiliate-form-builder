690ac2956bb9e2f6f4de71f5a9c687ce109424fe
arunlc <arun.k@spinircle.com>
2025-06-11 12:10:31 +0530

    Create urls.py

---
+++ apps/users/urls.py
@@ -0,0 +1,14 @@
+# apps/users/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+]
