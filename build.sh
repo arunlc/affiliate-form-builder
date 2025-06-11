18c5accec3c749708e156ee55504dc3771708fc4
arunlc <arun.k@spinircle.com>
2025-06-11 11:29:54 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Install Node.js dependencies and build frontend
+cd frontend
+npm ci
+npm run build
+cd ..
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run database migrations
+python manage.py migrate
