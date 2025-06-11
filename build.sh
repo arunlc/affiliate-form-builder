8be1e1698d63af4f06e7b5ef0078a6382721f85c
arunlc <arun.k@spinircle.com>
2025-06-11 19:18:11 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,12 +7,21 @@ pip install -r requirements.txt
 
 # Install Node.js dependencies and build frontend
 cd frontend
-npm install
+npm ci
 npm run build
 cd ..
 
 # Collect static files
 python manage.py collectstatic --no-input
 
+# Create migrations in the correct order (users first, then others)
+echo "Creating migrations..."
+python manage.py makemigrations users
+python manage.py makemigrations core
+python manage.py makemigrations forms
+python manage.py makemigrations affiliates
+python manage.py makemigrations leads
+
 # Run database migrations
+echo "Running migrations..."
 python manage.py migrate
