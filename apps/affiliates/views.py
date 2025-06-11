759031d39afe2d64e820b2cf4490e98d612b8399
arunlc <arun.k@spinircle.com>
2025-06-11 18:57:37 +0530

    Create views.py

---
+++ apps/affiliates/views.py
@@ -0,0 +1,26 @@
+# apps/affiliates/views.py
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Affiliate
+from .serializers import AffiliateSerializer
+
+class AffiliateViewSet(viewsets.ModelViewSet):
+    queryset = Affiliate.objects.all()
+    serializer_class = AffiliateSerializer
+    permission_classes = [IsAuthenticated]
+
+class AffiliateStatsView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, affiliate_id):
+        # TODO: Return affiliate statistics
+        return Response({'affiliate_id': affiliate_id, 'stats': 'placeholder'})
+
+class AffiliateLeadsView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, affiliate_id):
+        # TODO: Return affiliate leads
+        return Response({'affiliate_id': affiliate_id, 'leads': []})
