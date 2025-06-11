b60656a623a274d9033dadba6dbfbea9833753c8
arunlc <arun.k@spinircle.com>
2025-06-11 20:29:49 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,7 +12,16 @@ urlpatterns = [
     path('api/affiliates/', include('apps.affiliates.urls')),
     path('api/core/', include('apps.core.urls')),
     path('embed/', include('apps.forms.urls')),  # For embedded forms
+    
+    # Serve React frontend for all other routes
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Catch-all pattern for React router (SPA)
+# This should be last in the list
+urlpatterns += [
+    path('<path:path>', TemplateView.as_view(template_name='index.html'), name='react_routes'),
+]
