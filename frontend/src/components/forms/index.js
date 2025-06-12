37b73c26ec9209c5982929e74a1f34930e7f374a
arunlc <arun.k@spinircle.com>
2025-06-12 19:46:51 +0530

    Create index.js

---
+++ frontend/src/components/forms/index.js
@@ -0,0 +1,5 @@
+// frontend/src/components/forms/index.js
+export { default as FormModal } from './FormModal'
+export { default as FormCard } from './FormCard'
+export { default as FormStatsModal } from './FormStatsModal'
+export { default as FormFieldsPreview } from './FormFieldsPreview'
