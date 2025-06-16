73e09b229b994487c4aef10dd6182718a4ec7f4d
arunlc <arun.k@spinircle.com>
2025-06-16 21:18:23 +0530

    Update serializers.py

--- apps/forms/serializers.py
+++ apps/forms/serializers.py
@@ -13,6 +13,9 @@ class FormSerializer(serializers.ModelSerializer):
     fields = FormFieldSerializer(many=True, read_only=True)
     total_submissions = serializers.SerializerMethodField()
     new_leads_count = serializers.SerializerMethodField()
+    assigned_affiliates_count = serializers.SerializerMethodField()
+    my_submissions = serializers.SerializerMethodField()  # For affiliates
+    my_new_leads = serializers.SerializerMethodField()    # For affiliates
     
     class Meta:
         model = Form
@@ -21,9 +24,69 @@ class FormSerializer(serializers.ModelSerializer):
     
     def get_total_submissions(self, obj):
         """Get total number of submissions for this form"""
+        request = self.context.get('request')
+        if not request:
+            return obj.leads.count()
+        
+        # If user is affiliate, only count their submissions
+        if request.user.user_type == 'affiliate':
+            try:
+                from apps.affiliates.models import Affiliate
+                affiliate = Affiliate.objects.get(user=request.user)
+                return obj.leads.filter(affiliate=affiliate).count()
+            except:
+                return 0
+        
         return obj.leads.count()
     
     def get_new_leads_count(self, obj):
         """Get count of new leads in the last 24 hours"""
+        request = self.context.get('request')
         twenty_four_hours_ago = timezone.now() - timedelta(hours=24)
-        return obj.leads.filter(created_at__gte=twenty_four_hours_ago).count()
+        
+        queryset = obj.leads.filter(created_at__gte=twenty_four_hours_ago)
+        
+        # If user is affiliate, only count their leads
+        if request and request.user.user_type == 'affiliate':
+            try:
+                from apps.affiliates.models import Affiliate
+                affiliate = Affiliate.objects.get(user=request.user)
+                queryset = queryset.filter(affiliate=affiliate)
+            except:
+                return 0
+        
+        return queryset.count()
+    
+    def get_assigned_affiliates_count(self, obj):
+        """Get number of affiliates assigned to this form (Admin only)"""
+        request = self.context.get('request')
+        if request and request.user.user_type == 'admin':
+            return obj.assigned_affiliates.filter(is_active=True).count()
+        return None
+    
+    def get_my_submissions(self, obj):
+        """Get affiliate's total submissions for this form"""
+        request = self.context.get('request')
+        if request and request.user.user_type == 'affiliate':
+            try:
+                from apps.affiliates.models import Affiliate
+                affiliate = Affiliate.objects.get(user=request.user)
+                return obj.leads.filter(affiliate=affiliate).count()
+            except:
+                return 0
+        return None
+    
+    def get_my_new_leads(self, obj):
+        """Get affiliate's new leads for this form"""
+        request = self.context.get('request')
+        if request and request.user.user_type == 'affiliate':
+            try:
+                from apps.affiliates.models import Affiliate
+                affiliate = Affiliate.objects.get(user=request.user)
+                return obj.leads.filter(
+                    affiliate=affiliate,
+                    status='new'
+                ).count()
+            except:
+                return 0
+        return None
