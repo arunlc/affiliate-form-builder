04db2c40607b4e0c3b156bfa539c97b24816dd84
arunlc <arun.k@spinircle.com>
2025-06-17 19:12:52 +0530

    Update admin.py

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,14 +1,15 @@
-# apps/core/admin.py
+# apps/core/admin.py - FIXED VERSION
 from django.contrib import admin
-from .models import Setting, Analytics
+from .models import Setting
 
 @admin.register(Setting)
 class SettingAdmin(admin.ModelAdmin):
     list_display = ('key', 'value', 'updated_at')
     search_fields = ('key', 'description')
 
-@admin.register(Analytics)
-class AnalyticsAdmin(admin.ModelAdmin):
-    list_display = ('form', 'date', 'views', 'submissions', 'conversion_rate')
-    list_filter = ('date', 'form')
-    date_hierarchy = 'date'
+# Analytics model is commented out in models.py, so we don't import it
+# @admin.register(Analytics)
+# class AnalyticsAdmin(admin.ModelAdmin):
+#     list_display = ('form', 'date', 'views', 'submissions', 'conversion_rate')
+#     list_filter = ('date', 'form')
+#     date_hierarchy = 'date'
